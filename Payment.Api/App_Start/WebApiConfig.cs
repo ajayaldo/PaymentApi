b166aac15f874907e1abfd03749e00d22eb84fe6
Ajay Mekkonath Viswanathan <ajayaldo@gmail.com>
2018-06-25 09:36:05 +1000

    Add project files.

---
+++ Payment.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,15 @@
+﻿using Payment.Api.Logger;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+
+namespace Payment.Api
+{
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
+    {
+      config.Services.Add(typeof(IExceptionLogger), new GlobalExceptionLogger());
+      config.MapHttpAttributeRoutes();
+    }
+  }
+}
---
+++ Payment.Api/Controllers/PaymentController.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.Http;
+
+namespace Payment.Api.Controllers
+{
+  public class PaymentController
+    : ApiController
+  { }
+}
